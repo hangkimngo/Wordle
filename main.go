a8dbe710b0529b862fdd27282294f87c84ab09e6
Kim Hằng Ngô <k-kngo@c2r3p7.hive.fi>
2026-02-04 18:10:34 +0200

    fix

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 	index, err := strconv.Atoi(index_word)
 
 	if err != nil {
-		fmt.Println("Index must be a number.")
+		fmt.Println("Invalid command-line argument. Please launch with a valid number.")
 		return
 	}
 

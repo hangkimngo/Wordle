8cc702c491a34ecf585afd8f6364c7757ad7002d
Kim Hằng Ngô <k-kngo@c2r3p1.hive.fi>
2026-02-05 11:56:23 +0200

    fix

--- game/lencheck.go
+++ game/lencheck.go
@@ -4,10 +4,10 @@ import "fmt"
 
 func LenCheck(game *Game) bool {
 	if len(game.guess_letter) == 0 {
-		fmt.Println("Make a guess!")
+
 		return false
 	} else if len(game.guess_letter) != 5 {
-		fmt.Println("  Your guest must be exactly 5 letters long.")
+		fmt.Println("Your guest must be exactly 5 letters long.")
 		return false
 	}
 	return true
